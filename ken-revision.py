ea4469b2900d5335d7fa19c07c965d724e05da6e
jhwi234 <jhwi234@uky.edu>
2023-12-10 13:26:46 -0500

    added brown and clemt3

--- ken-revision.py
+++ ken-revision.py
@@ -1,24 +1,20 @@
-import logging
 import random
+import logging
 import re
 from pathlib import Path
 import enum
 import heapq
+import numpy as np
 
 import kenlm
-import numpy as np
 import nltk
 import subprocess
 from retry import retry
 
-def softmax(x):
-    """Compute softmax values for each set of scores in x."""
-    e_x = np.exp(x - np.max(x))
-    return e_x / e_x.sum(axis=0)
-
 # Constants
 VOWELS = 'aeiou'
 CONSONANTS = ''.join(set('abcdefghijklmnopqrstuvwxyz') - set(VOWELS))
+seed = 42
 
 # Enumeration for letter types
 class LetterType(enum.Enum):
@@ -26,7 +22,6 @@ class LetterType(enum.Enum):
     CONSONANT = 2
 
 logging.basicConfig(level=logging.INFO)
-clean_pattern = re.compile(r'\b[a-zA-Z]{4,}\b')
 
 @retry(subprocess.CalledProcessError, tries=3, delay=2)
 def model_task(corpus_name, q, corpus_path, model_directory):
@@ -48,95 +43,60 @@ def model_task(corpus_name, q, corpus_path, model_directory):
         raise
 
 class LanguageModel:
-    def __init__(self, q_range=(6, 6), split_config=0.9):
+    def __init__(self, corpus_name, q_range=(6, 6), split_config=0.9, seed=42):
+        self.corpus_name = corpus_name
         self.q_range = range(q_range[0], q_range[1] + 1)
         self.model = {}
         self.corpus = set()
         self.test_set = set()
         self.training_set = set()
         self.all_words = set()
+        self.rng = random.Random(seed)
         self.split_config = split_config
 
+    def load_corpus(self, corpus_source, is_file=False):
+        if is_file:
+            with open(corpus_source, 'r', encoding='utf-8') as file:
+                text_data = file.read()
+        else:
+            text_data = ' '.join(corpus_source())
+
+        tokenized_data = nltk.word_tokenize(text_data)
+        self.corpus = self.clean_text(' '.join(tokenized_data))
+
     def clean_text(self, text: str) -> set[str]:
-        return set(clean_pattern.findall(text))
+        clean_pattern = re.compile(r'\b[a-zA-Z]{4,}\b')
+        return set(word.lower() for word in clean_pattern.findall(text))
 
-    def load_corpus(self):
-        nltk.download('cmudict')
-        cmu_words = nltk.corpus.cmudict.words()
-        cmu_words = [word.lower() for word in cmu_words]
-        self.corpus = self.clean_text(' '.join(cmu_words))
-
-    # Modify the prepare_datasets function
     def prepare_datasets(self, min_word_length=None, max_word_length=None, vowel_replacement_ratio=0.5, consonant_replacement_ratio=0.5):
-        # Filter corpus based on word length if specified
+        # Use a separate variable for the filtered corpus to retain the original corpus.
+        filtered_corpus = self.corpus
+
+        # Filter the corpus based on word length if specified.
         if min_word_length or max_word_length:
-            self.corpus = {word for word in self.corpus if (min_word_length or 0) <= len(word) <= (max_word_length or float('inf'))}
+            filtered_corpus = {word for word in self.corpus if (min_word_length or 0) <= len(word) <= (max_word_length or float('inf'))}
 
-        total_size = len(self.corpus)
+        total_size = len(filtered_corpus)
         training_size = int(total_size * self.split_config)
-        shuffled_corpus = list(self.corpus)
-        random.shuffle(shuffled_corpus)
+        shuffled_corpus = list(filtered_corpus)
+        self.rng.shuffle(shuffled_corpus)
 
+        # Define training and test sets based on the filtered (or original) corpus.
         self.training_set = set(shuffled_corpus[:training_size])
         unprocessed_test_set = set(shuffled_corpus[training_size:])
-        self.save_set_to_file(self.training_set, "cmu_training_set.txt")
 
-        # Update the test set preparation to include the original word
+        # Save the training set to a file.
+        self.save_set_to_file(self.training_set, f"{self.corpus_name}_training_set.txt")
+
+        # Update the test set preparation to include the original word.
         self.test_set = {self.replace_random_letter(word, include_original=True, vowel_replacement_ratio=vowel_replacement_ratio, consonant_replacement_ratio=consonant_replacement_ratio) for word in unprocessed_test_set}
-        self.save_set_to_file(self.test_set, "formatted_test_set.txt")
-        # Combine training and test sets into all_words
+        self.save_set_to_file(self.test_set, f"{self.corpus_name}_formatted_test_set.txt")
+
+        # Combine training and test sets into all_words.
         self.all_words = self.training_set.union(self.test_set)
 
-    def prepare_and_save_test_set(self, data_set, file_name):
-        formatted_test_set = []
-        for word in data_set:
-            modified_word, missing_letter = self.replace_random_letter(word)
-            # Append a tuple containing the modified word, the missing letter, and the original word
-            formatted_test_set.append((modified_word, missing_letter, word))
-
-        self.save_set_to_file(formatted_test_set, file_name)
-
-    def replace_random_letter(self, word, include_original=False, vowel_replacement_ratio=0.5, consonant_replacement_ratio=0.5):
-        # Create a dictionary to categorize indices of vowels and consonants in the word.
-        categorized_indices = {LetterType.VOWEL: [], LetterType.CONSONANT: []}
-
-        # Loop through each letter in the word.
-        for i, letter in enumerate(word):
-            # Determine if the letter is a vowel or a consonant.
-            letter_type = LetterType.VOWEL if letter in VOWELS else LetterType.CONSONANT
-            # Add the index of the letter to the corresponding category (vowel or consonant).
-            categorized_indices[letter_type].append(i)
-
-        # Decide whether to replace a vowel or a consonant based on their presence and specified ratios.
-        replace_vowel = len(categorized_indices[LetterType.VOWEL]) > 0 and random.random() < vowel_replacement_ratio
-        replace_consonant = len(categorized_indices[LetterType.CONSONANT]) > 0 and random.random() < consonant_replacement_ratio
-
-        # If both vowel and consonant are candidates for replacement, randomly choose one.
-        if replace_vowel and replace_consonant:
-            replace_vowel = random.choice([True, False])
-            replace_consonant = not replace_vowel
-
-        # Choose a random index from the selected category (vowel or consonant) for replacement.
-        if replace_vowel:
-            letter_index = random.choice(categorized_indices[LetterType.VOWEL])
-        elif replace_consonant:
-            letter_index = random.choice(categorized_indices[LetterType.CONSONANT])
-        else:
-            # If no vowels or consonants are eligible, pick any random letter in the word.
-            letter_index = random.randint(0, len(word) - 1)
-
-        # Store the letter to be replaced and create the modified word with a placeholder '_' at the replaced letter's position.
-        missing_letter = word[letter_index]
-        modified_word = word[:letter_index] + '_' + word[letter_index + 1:]
-
-        # Return a tuple with the modified word and missing letter. If 'include_original' is True, include the original word as well.
-        if include_original:
-            return (modified_word, missing_letter, word)
-        else:
-            return modified_word, missing_letter
-
     def generate_and_load_models(self):
-        training_corpus_path = self.generate_formatted_corpus(self.training_set, 'cmu_formatted_training_corpus.txt')
+        training_corpus_path = self.generate_formatted_corpus(self.training_set, f"{self.corpus_name}_formatted_training_set.txt")
         self.generate_models_from_corpus(training_corpus_path)
 
     def generate_formatted_corpus(self, data_set, path):
@@ -148,31 +108,73 @@ class LanguageModel:
         return path
 
     def generate_models_from_corpus(self, corpus_path):
-        model_directory = Path('cmu_models')
+        model_directory = Path(f'{self.corpus_name}_models')
         model_directory.mkdir(parents=True, exist_ok=True)
 
         for q in self.q_range:
-            _, binary_file = model_task('cmu', q, corpus_path, model_directory)
+            _, binary_file = model_task(self.corpus_name, q, corpus_path, model_directory)
             if binary_file:
                 self.model[q] = kenlm.Model(binary_file)
                 logging.info(f"Model for {q}-gram loaded.")
 
-    # no entropy weighting
+    def prepare_and_save_test_set(self, data_set, file_name):
+        formatted_test_set = []
+        for word in data_set:
+            modified_word, missing_letter = self.replace_random_letter(word)
+            # Append a tuple containing the modified word, the missing letter, and the original word
+            formatted_test_set.append((modified_word, missing_letter, word))
+
+        self.save_set_to_file(formatted_test_set, file_name)
+
+    def replace_random_letter(self, word, include_original=False, vowel_replacement_ratio=0.5, consonant_replacement_ratio=0.5):
+        # Categorize indices of vowels and consonants in the word.
+        categorized_indices = {
+            LetterType.VOWEL: [i for i, letter in enumerate(word) if letter in VOWELS],
+            LetterType.CONSONANT: [i for i, letter in enumerate(word) if letter in CONSONANTS]
+        }
+
+        # Randomly decide whether to replace a vowel and/or a consonant.
+        replace_vowel = self.rng.random() < vowel_replacement_ratio and categorized_indices[LetterType.VOWEL]
+        replace_consonant = self.rng.random() < consonant_replacement_ratio and categorized_indices[LetterType.CONSONANT]
+
+        # Select the type of letter to replace.
+        if replace_vowel and replace_consonant:
+            letter_type = self.rng.choice([LetterType.VOWEL, LetterType.CONSONANT])
+        elif replace_vowel:
+            letter_type = LetterType.VOWEL
+        elif replace_consonant:
+            letter_type = LetterType.CONSONANT
+        else:
+            # Fallback if no letter is selected for replacement.
+            letter_type = self.rng.choice([LetterType.VOWEL, LetterType.CONSONANT])
+            if not categorized_indices[letter_type]:
+                # Fallback to any letter if the chosen type is not in the word.
+                letter_type = LetterType.VOWEL if letter_type == LetterType.CONSONANT else LetterType.CONSONANT
+
+        # Choose a random index from the selected letter type for replacement.
+        letter_indices = categorized_indices[letter_type]
+        letter_index = self.rng.choice(letter_indices)
+
+        # Replace the selected letter with a placeholder and store the missing letter.
+        missing_letter = word[letter_index]
+        modified_word = word[:letter_index] + '_' + word[letter_index + 1:]
+
+        # Return the results.
+        if include_original:
+            return (modified_word, missing_letter, word)
+        else:
+            return modified_word, missing_letter
+
     def predict_missing_letter(self, test_word):
-        # Identify the position of the missing letter (denoted by '_').
         missing_letter_index = test_word.index('_')
-
-        # Initialize a dictionary to store the log probabilities of each alphabet letter being the missing one.
-        log_probabilities = {letter: [] for letter in 'abcdefghijklmnopqrstuvwxyz'}
-
-        # Add start (<s>) and end (</s>) boundary markers to the word.
+        probabilities = {letter: [] for letter in 'abcdefghijklmnopqrstuvwxyz'}
         test_word_with_boundaries = f"<s> {test_word} </s>"
+        lambda_weights = self.calculate_lambda_weights()
 
         for q in self.q_range:
-            if q not in self.model:
+            model = self.model.get(q)
+            if not model:
                 continue
-
-            model = self.model[q]
 
             # Determine context size.
             left_size = min(missing_letter_index + 1, q - 1)
@@ -189,22 +191,32 @@ class LanguageModel:
             left_context_joined = ' '.join(left_context)
             right_context_joined = ' '.join(right_context)
 
-            # For each possible letter, calculate the log probability of it being the correct one to fill the missing spot.
+            # Probability calculation using list comprehension
             for letter in 'abcdefghijklmnopqrstuvwxyz':
                 full_sequence = f"{left_context_joined} {letter} {right_context_joined}".strip()
-                log_prob_full = model.score(full_sequence)
-                log_probabilities[letter].append(log_prob_full)
+                prob_full = np.exp(model.score(full_sequence))  # Convert log probability to linear probability
+                probabilities[letter].append(prob_full * lambda_weights[q])
 
-        # Average the log probabilities across all q values.
-        averaged_log_probabilities = {}
-        for letter, log_probs_list in log_probabilities.items():
-            if log_probs_list:
-                averaged_log_prob = np.mean(log_probs_list)
-                averaged_log_probabilities[letter] = averaged_log_prob
+        # Interpolation using dictionary comprehension
+        interpolated_probabilities = {
+            letter: sum(probs_list)
+            for letter, probs_list in probabilities.items() if probs_list
+        }
 
-        # Select the top three letters with the highest probabilities.
-        top_three_predictions = heapq.nlargest(3, averaged_log_probabilities.items(), key=lambda item: item[1])
-        return [(letter, np.exp(log_prob)) for letter, log_prob in top_three_predictions]
+        # Efficient selection of top three predictions
+        return heapq.nlargest(3, interpolated_probabilities.items(), key=lambda item: item[1])
+
+    def calculate_lambda_weights(self):
+        # Define lambda weights for each n-gram size. These should sum up to 1.
+        lambda_weights = {
+            1: 0.0,  # Unigram
+            2: 0.2,  # Bigram
+            3: 0.2,  # Trigram
+            4: 0.2,
+            5: 0.2,
+            6: 0.2
+        }
+        return lambda_weights
 
     def evaluate_model(self, output_file):
         correct_counts = {1: 0, 2: 0, 3: 0}
@@ -286,26 +298,42 @@ class LanguageModel:
         else:
             return f"{item}"
 
+def load_nltk_corpus(corpus):
+    nltk.download(corpus)
+    return getattr(nltk.corpus, corpus).words()
+
+def load_file_corpus(filepath):
+    with open(filepath, 'r', encoding='utf-8') as file:
+        return file.read().split()
+
 def main():
-    random.seed(23)
-    logging.info("Starting the main function")
+    nltk.download('punkt')
 
-    lm = LanguageModel()
-    logging.info("Language model initialized")
+    # Process CMU Dict Corpus
+    process_corpus("cmudict", lambda: load_nltk_corpus('cmudict'))
 
-    lm.load_corpus()
-    logging.info("CMU corpus loaded")
+    # Process Brown Corpus
+    process_corpus("brown", lambda: load_nltk_corpus('brown'))
+
+    # Process text file
+    process_corpus("CLMET3", lambda: load_file_corpus("CLMET3_words.txt"))
+
+def process_corpus(corpus_name, corpus_loader):
+    lm = LanguageModel(corpus_name)
+    logging.info(f"{corpus_name} Language model initialized")
+
+    lm.load_corpus(corpus_loader)
+    logging.info(f"{corpus_name} corpus loaded")
 
     lm.prepare_datasets()
     logging.info(f"Training set prepared with {len(lm.training_set)} words")
     logging.info(f"Test set prepared with {len(lm.test_set)} words")
 
     lm.generate_and_load_models()
-    logging.info("Q-gram models generated and loaded")
+    logging.info(f"{corpus_name} Q-gram models generated and loaded")
 
-    correct_counts, top1_recall, top2_recall, top3_recall = lm.evaluate_model("cmu_predictions.txt")
-
-    logging.info("Model evaluation completed")
+    correct_counts, top1_recall, top2_recall, top3_recall = lm.evaluate_model(f"{corpus_name}_predictions.txt")
+    logging.info(f"Model evaluation completed for {corpus_name}")
     logging.info(f"TOP1 PRECISION: {correct_counts[1] / len(lm.test_set):.2%}")
     logging.info(f"TOP2 PRECISION: {correct_counts[2] / len(lm.test_set):.2%}")
     logging.info(f"TOP3 PRECISION: {correct_counts[3] / len(lm.test_set):.2%}")
@@ -315,4 +343,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
--- miscstuff.py
+++ miscstuff.py
@@ -1,6 +1,6 @@
 import nltk
 from string import ascii_lowercase
-
+'''
 # Ensure the Brown corpus is downloaded
 nltk.download('brown')
 brown_words = nltk.corpus.brown.words()
@@ -82,3 +82,41 @@ for letter, percentage in sorted(token_letter_percentage.items(), key=lambda ite
 print("\nType Letter Percentages:")
 for letter, percentage in sorted(type_letter_percentage.items(), key=lambda item: item[1], reverse=True):
     print(f"{letter}: {percentage:.2f}%")
+'''
+import nltk
+import re
+from typing import Set
+
+# Ensure NLTK corpora are downloaded
+nltk.download('brown')
+nltk.download('cmudict')
+
+def process_words(corpus_words: Set[str]) -> Set[str]:
+    # Remove punctuation/non-alphanumeric characters and numbers
+    words = {re.sub(r'[^a-zA-Z]', '', word) for word in corpus_words}
+    return {word for word in words if word.isalpha()}
+
+def read_file_words(file_path: str) -> Set[str]:
+    with open(file_path, 'r') as file:
+        return set(file.read().lower().split())
+
+def count_corpus_words(corpus_name: str, corpus_words: Set[str]) -> None:
+    processed_words = process_words(corpus_words)
+    print(f"There are {len(processed_words)} unique words in the {corpus_name} corpus.")
+
+def main():
+    # Process and count words from the Brown corpus
+    brown_words = set(word.lower() for word in nltk.corpus.brown.words())
+    count_corpus_words("Brown", brown_words)
+
+    # Process and count words from the CMU Pronouncing Dictionary
+    cmu_words = set(word.lower() for word, _ in nltk.corpus.cmudict.entries())
+    count_corpus_words("CMU", cmu_words)
+
+    # Process and count words from CLMET3_words.txt
+    clmet3_words = read_file_words('CLMET3_words.txt')
+    count_corpus_words("CLMET3", clmet3_words)
+
+if __name__ == "__main__":
+    main()
+
