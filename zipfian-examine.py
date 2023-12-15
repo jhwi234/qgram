ef7d2c7a4c3d903469b08739252638120e5ffb35
jhwi234 <jhwi234@uky.edu>
2023-12-15 11:17:18 -0500

    add zipfian-examine.py

---
+++ zipfian-examine.py
@@ -0,0 +1,37 @@
+import nltk
+from collections import Counter
+
+def calculate_zipfian_probabilities(corpus_name):
+    nltk.download(corpus_name)
+    corpus = getattr(nltk.corpus, corpus_name).words()
+    words = [word.lower() for word in corpus if word.isalpha()]
+    word_freq = Counter(words)
+    total_words = sum(word_freq.values())
+    unique_word_types = len(word_freq)
+
+    sorted_freqs = sorted(word_freq.values(), reverse=True)
+    cumulative_freq = 0
+    half_cumulative_freq = total_words / 2
+    top_half_count = 0
+
+    for freq in sorted_freqs:
+        cumulative_freq += freq
+        top_half_count += 1
+        if cumulative_freq >= half_cumulative_freq:
+            break
+
+    prob_top_half = cumulative_freq / total_words
+    prob_bottom_half = (total_words - cumulative_freq) / total_words
+
+    return prob_top_half, prob_bottom_half, top_half_count, total_words, unique_word_types
+
+corpora = ['brown', 'gutenberg', 'reuters', 'webtext', 'inaugural']
+
+for corpus_name in corpora:
+    prob_top_half, prob_bottom_half, top_half_count, total_words, unique_word_types = calculate_zipfian_probabilities(corpus_name)
+    print(f"Corpus: {corpus_name}")
+    print(f"Probability of a word being in the top half of most common words: {prob_top_half:.4f}")
+    print(f"Probability of a word being in the bottom half of most common words: {prob_bottom_half:.4f}")
+    print(f"Number of words in the top half: {top_half_count}")
+    print(f"Total number of words in the corpus: {total_words}")
+    print(f"Number of unique word types in the corpus: {unique_word_types}\n")
